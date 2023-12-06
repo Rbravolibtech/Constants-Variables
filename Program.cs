defe61128c7292c57196b7efef5102ad06f36268
Rbravolibtech <122310916+Rbravolibtech@users.noreply.github.com>
2023-12-06 14:15:02 -0700

    coding on variables and constants

--- Program.cs
+++ Program.cs
@@ -1,3 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
 
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 2;
+            int count = 10;
+            float totalPrice = 20.95f;
+            char character = 'A';
+            string firstName = "Mosh";
+            bool isWorking = false;
+
+            Console.WriteLine(number);
+            Console.WriteLine(count);
+            Console.WriteLine(totalPrice);
+            Console.WriteLine(character);
+            Console.WriteLine(firstName);
+            Console.WriteLine(isWorking);
+
+            //Console.WriteLine("{0} {1}", byte.MaxValue, byte.MaxValue);
+            //Console.WriteLine("{0} {1}", float.MaxValue, float.MaxValue);
+            //const float Pi = 3.14f;
+            //Pi 
+        }
+    }
+}
